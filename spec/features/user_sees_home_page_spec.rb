3eb52235c4f1bf6e8e19285cbaee53889cbf1a26
admande <adam.mandeville@gmail.com>
2015-12-28 15:31:04 -0500

    added nav bars, added image to home page

--- spec/features/user_sees_home_page_spec.rb
+++ spec/features/user_sees_home_page_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 feature "user sees home page" do
   scenario "user visits home page" do
-    visit '/static_pages/home'
+    visit '/'
     expect(page).to have_content "This is the home page for the Ruby on Rails"
-    expect(page).to have_title "Home | Ruby on Rails Tutorial Sample App"
+    expect(page).to have_title "Ruby on Rails Tutorial Sample App"
   end
 end
